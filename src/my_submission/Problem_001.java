8a5f7bc2247516cd34b63342052788ab9001077b
Mao Charles <charles.mao@sap.com>
2018-01-22 19:21:12 +0800

    second commit

--- src/my_submission/Problem_001.java
+++ src/my_submission/Problem_001.java
@@ -1,3 +1,4 @@
+package my_submission;
 /**
  * 1. Two Sum
  * 
@@ -10,23 +11,19 @@
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
  *
+ *@date 2017-11-28
+ *@author Charles Mao
  */
 public class Problem_001 {
 
 	public static void main(String[] args) {
 		int[] num = {2, 7, 11, 15};
-		int[] num1 = {3,2,4};
 		int[] sum;
 		sum = twoSum(num,9);
 		for(int n: sum){
 			System.out.println(n);
 		}
 		
-		System.out.println();
-		sum = twoSum(num1,6);
-		for(int n: sum){
-			System.out.println(n);
-		}
 	}
 	
 	public static int[] twoSum(int[] nums, int target) {
